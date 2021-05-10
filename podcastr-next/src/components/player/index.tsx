0e5b55c28f2417c046f7b8dbcb5efbc685d1db10
Arthur <arthur001123@gmail.com>
2021-05-09 21:19:15 -0300

    adicionando dados do episódio ao player enquanto tocando

--- podcastr-next/src/components/player/index.tsx
+++ podcastr-next/src/components/player/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
+import Image from "next/image";
 
 export default function Player() {
   const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
@@ -14,11 +15,24 @@ export default function Player() {
         <div> Tocando Agora {episode?.title}</div>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um PodCast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um PodCast para ouvir</strong>
+        </div>
+      )}
 
-      <footer className={styles.empty}>
+      <footer className={episode ? "" : styles.empty}>
         <div className={styles.progress}>
           <span>0:00</span>
           <div className={styles.slider}>
